6e8b6bade6e563f383ee4b22c1815a1f4ad31761
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-06 18:05:06 +0530

    feat: add initial Terraform configuration with AWS provider and EC2 instance resource

---
+++ custom-key/main.tf
@@ -0,0 +1,13 @@
+resource "aws_key_pair" "my_key"{
+    key_name = "my_key"
+    public_key = file(/root/.ssh/id_ed25519.pub)
+}
+
+resource "aws_instance" "ec2"{
+    ami = "ami-0199ac7c9fbf9ed83"
+    instance_type = "t3.micro"
+    key_name = aws_key_pair.my_key.key_name
+    tags = {
+        Name = "tf-instance"
+    }
+}
