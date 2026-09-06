6ce90b7d077e54aaf2d1be14f0cc7a98bc56e345
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-06 18:22:40 +0530

    fix: update user_data_base64 to use filebase64 function for EC2 instance

--- user-data/main.tf
+++ user-data/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2"{
     ami = "ami-0199ac7c9fbf9ed83"
     instance_type = "t3.micro"
     key_name = aws_key_pair.my_key.key_name
-    user_data_base64 = file("/root/Terraform-revise/user-data/user_data.sh")
+    user_data_base64 = filebase64("/root/Terraform-revise/user-data/user_data.sh")
     tags = {
         Name = "tf-instance"
     }
