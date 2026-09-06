b4f2032c6b4395fe2052b7103bb9c41dbeace855
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-06 17:54:36 +0530

    feat: add initial Terraform configuration with AWS provider, EC2 instance, and variables

---
+++ Day02/var.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+    default = "ami-0199ac7c9fbf9ed83"
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "key_name" {
+    default = "hyderabad"
+}
+variable "tag" {
+    default = "tf-instance"
+}
